8c8bbfa19d853b1c1842da94a6b6a8adf3836c61
Jan Suchotzki <jan@lernmoment.de>
2016-07-14 22:31:38 +0200

    Fügt Spielfelder und deren Ausgabe hinzu
    Lösung zu Übung 3

--- TicTacToe/Spielbrett.cs
+++ TicTacToe/Spielbrett.cs
@@ -4,14 +4,22 @@ namespace TicTacToe
 {
     class Spielbrett
     {
+        private static string[] spalteA = { " ", " ", " " };
+        private static string[] spalteB = { " ", " ", " " };
+        private static string[] spalteC = { " ", " ", " " };
+
         public static void Anzeigen()
         {
-            Console.WriteLine(" A B C");
-            Console.WriteLine("0 | | ");
-            Console.WriteLine(" -+-+-");
-            Console.WriteLine("1 | | ");
-            Console.WriteLine(" -+-+-");
-            Console.WriteLine("2 | | ");
+            string zeileNull = "0 " + spalteA[0] + " | " + spalteB[0] + " | " + spalteC[0] + " ";
+            string zeileEins = "0 " + spalteA[1] + " | " + spalteB[1] + " | " + spalteC[1] + " ";
+            string zeileZwei = "0 " + spalteA[2] + " | " + spalteB[2] + " | " + spalteC[2] + " ";
+
+            Console.WriteLine("  A   B   C ");
+            Console.WriteLine(zeileNull);
+            Console.WriteLine(" ---+---+---");
+            Console.WriteLine(zeileEins);
+            Console.WriteLine(" ---+---+---");
+            Console.WriteLine(zeileZwei);
         }
     }
 }
