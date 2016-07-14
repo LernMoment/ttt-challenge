2d5cbb07f96bad66acbdc67a7e0f84d5fd201378
Jan Suchotzki <jan@lernmoment.de>
2016-07-14 22:49:47 +0200

    Behebt die falsche Ausgabe der
    Zeilennummern. Nachtrag zu Übung 3.

--- TicTacToe/Spielbrett.cs
+++ TicTacToe/Spielbrett.cs
@@ -49,8 +49,8 @@ namespace TicTacToe
         public static void Anzeigen()
         {
             string zeileNull = "0 " + spalteA[0] + " | " + spalteB[0] + " | " + spalteC[0] + " ";
-            string zeileEins = "0 " + spalteA[1] + " | " + spalteB[1] + " | " + spalteC[1] + " ";
-            string zeileZwei = "0 " + spalteA[2] + " | " + spalteB[2] + " | " + spalteC[2] + " ";
+            string zeileEins = "1 " + spalteA[1] + " | " + spalteB[1] + " | " + spalteC[1] + " ";
+            string zeileZwei = "2 " + spalteA[2] + " | " + spalteB[2] + " | " + spalteC[2] + " ";
 
             Console.WriteLine("  A   B   C ");
             Console.WriteLine(zeileNull);
