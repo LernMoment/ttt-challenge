dacf60ab2c3aea49a37d97fd39746468703208a4
Jan Suchotzki <jan@lernmoment.de>
2016-08-01 13:19:45 +0200

    Beschreibt IstSpielzug-Algo in Pseudocode

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -34,8 +34,31 @@ namespace TicTacToe
             Console.ReadLine();
         }
 
+        /// <summary>
+        /// Überprüft ob es sich bei dem angegebenen Kommando um einen Spielzug handelt.
+        /// Das ist der Fall, wenn das Kommando aus einem Buchstaben (an erster Stelle)
+        /// und einer Zahl (an zweiter Stelle) besteht.
+        /// </summary>
+        /// <param name="kommando">Entweder ein Spielzug, oder eines der anderen gültigen
+        /// Kommandos</param>
+        /// <returns>true, wenn es sich um einen Spielzug (egal ob gültig oder nicht) handelt;
+        /// false, wenn es sich um ein anderes (oder nicht unterstütztes) Kommando handelt.</returns>
         static bool IstSpielzug(string kommando)
         {
+            // 1) Prüfen ob das Kommando länger als 2 Zeichen ist.
+            // Ein Spielzug besteht immer aus exakt 2 Zeichen. Wenn das Kommando mehr Zeichen
+            // enthält, dann kann es kein Spielzug sein.
+
+            // 2) Prüfen ob das Kommando als erstes einen Buchstaben enthält.
+            // Ist das Kommando ein Spielzug, dann ist das erste Zeichen ein Buchstaben und 
+            // identifiziert die Spalte der Koordinate. Ist das erste Zeichen kein Buchstabe,
+            // kann das Kommando NICHT ein Spielzug sein.
+
+            // 3) Prüfen ob das Kommando an zweiter Stelle eine Ziffer enthält.
+            // Ist das Kommando ein Spielzug, dann ist das zweite Zeichen eine Ziffer und 
+            // identifiziert die Zeile der Koordinate. Ist das zweite Zeichen keine Ziffer,
+            // kann das Kommando NICHT ein Spielzug sein.
+
             return false;
         }
     }
